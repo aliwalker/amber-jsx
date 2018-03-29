fcede27aafac393dd6236445c2de9330d0557cf8
Alieeeeen <734943462@qq.com>
2018-03-29 18:16:34 +0800

    add test

--- build/test/writer.test.js
+++ build/test/writer.test.js
@@ -10,14 +10,49 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 describe('Writer', () => {
   describe('#_splitXML', () => {
-    it('basic test', () => {
+    it('basic case', () => {
       const example = `
       const element = (<div>Elem</div>);`;
       const writer = new _writer.Writer();
       const parts = writer._splitXML(example);
 
+      _assert2.default.equal(3, parts.length);
       _assert2.default.equal(_writer.JS, parts[0].type);
       _assert2.default.equal(_writer.XML, parts[1].type);
+    });
+
+    it('a little bit more', () => {
+      const example = `
+      function Clock(props) {
+        return (
+          <div id="app" onClick={handler}>
+            <h1>Hello, world!</h1>
+            <h2>It is {message}.</h2>
+          </div>
+          );
+      }
+      
+      const element = (<Clock />);`;
+      const writer = new _writer.Writer();
+      const parts = writer._splitXML(example);
+
+      _assert2.default.equal(5, parts.length);
+    });
+  });
+
+  describe('#_emitText', () => {
+    it('static text', () => {
+      const writer = new _writer.Writer();
+      const res = writer._emitText({ text: "This is static" });
+
+      _assert2.default.equal('"This is static"', res);
+    });
+
+    it('dynamic', () => {
+      const writer = new _writer.Writer();
+      const res = writer._emitText({ dText: "dynamic" });
+
+      _assert2.default.equal('dynamic', res);
     });
   });
 });
--- src/test/writer.test.js
+++ src/test/writer.test.js
@@ -3,14 +3,49 @@ import assert from 'assert';
 
 describe('Writer', () => {
   describe('#_splitXML', () => {
-    it('basic test', () => {
+    it('basic case', () => {
       const example = `
       const element = (<div>Elem</div>);`;
       const writer = new Writer();
       const parts = writer._splitXML(example);
 
+      assert.equal(3, parts.length);
       assert.equal(JS, parts[0].type);
       assert.equal(XML, parts[1].type);
+    });
+
+    it('a little bit more', () => {
+      const example = `
+      function Clock(props) {
+        return (
+          <div id="app" onClick={handler}>
+            <h1>Hello, world!</h1>
+            <h2>It is {message}.</h2>
+          </div>
+          );
+      }
+      
+      const element = (<Clock />);`;
+      const writer = new Writer();
+      const parts = writer._splitXML(example);
+
+      assert.equal(5, parts.length);
+    });
+  });
+
+  describe('#_emitText', () => {
+    it('static text', () => {
+      const writer = new Writer();  
+      const res = writer._emitText({ text: "This is static" });
+    
+      assert.equal('"This is static"', res);
+    });
+
+    it('dynamic', () => {
+      const writer = new Writer();
+      const res = writer._emitText({ dText: "dynamic" });
+
+      assert.equal('dynamic', res);
     });
   });
 })
